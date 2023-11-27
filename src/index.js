a086f495554de1a40026eaa0392491a0dd3620e5
Ayden Xiao <44585102+aydenxiao-gep@users.noreply.github.com>
2023-11-27 09:29:20 -0500

    added decrypt

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const prompt = require('prompt-sync')();
+var CryptoJS = require('crypto-js');
 var AES = require('crypto-js/aes');
 var KEY = 'ayden';
-var text = prompt('Please input plaintext here: ');
-var tokens = '';
-if(text.indexOf('access_token=') > -1) {
-    tokens = extractTokens(text);
+var actionType = prompt('Action type: 1 - Encrypt; 2 - Decrypt; :(1) ');
+if(!actionType) actionType = 1;
+if(actionType == 1) {
+    let text = prompt('Please input plaintext here: ');
+    let tokens = '';
+    if(text.indexOf('access_token=') > -1) {
+        tokens = extractTokens(text);
+    } else {
+        tokens = AES.encrypt(text, KEY).toString()
+    }
+    console.log(tokens);
 } else {
-    tokens = AES.encrypt(text, KEY).toString()
+    let ciphertext = prompt('Please input ciphertext here: ');
+    let text = decrypt(ciphertext);
+    console.log(text)
 }
-
-console.log(tokens);
 
 function extractTokens(orginal) {
     let tokens = [];
@@ -18,4 +26,9 @@ function extractTokens(orginal) {
         tokens.push(AES.encrypt(tk, KEY).toString());
     });
     return tokens;
+}
+
+function decrypt(ciphertext) {
+    let bytes = AES.decrypt(ciphertext, KEY);
+    return bytes.toString(CryptoJS.enc.Utf8);
 }
